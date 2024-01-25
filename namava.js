88d4af7fd905a5d25d3c386cf01812e6be5949cd
DELL <raminweb1401@gmail.com>
2024-01-25 23:23:20 +0330

    add menu

---
+++ namava.js
@@ -0,0 +1,16 @@
+const menuBtn = document.getElementById('menuBtn')
+const drop = document.querySelector('.dropdown')
+const back = document.querySelector('.backmenu')
+
+
+menuBtn.addEventListener('click',()=>{
+       drop.classList.add('show')
+      back.classList.add('show')
+})
+
+back.addEventListener('click',()=>{
+    drop.classList.remove('show')
+    back.classList.remove('show')
+})
+
+

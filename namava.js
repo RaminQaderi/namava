99b28056046f903e9aa35c1d3405cf2791d3e83d
DELL <raminweb1401@gmail.com>
2024-01-27 22:22:46 +0330

    make in sliders

--- namava.js
+++ namava.js
@@ -1,7 +1,11 @@
 const menuBtn = document.getElementById('menuBtn')
 const drop = document.querySelector('.dropdown')
 const back = document.querySelector('.backmenu')
-
+const slider = document.querySelector('.banner')
+const slide = document.querySelectorAll('.slide')
+const btnleft=document.querySelector('.icons')
+const btnRight=document.querySelector('.icons2')
+let useslider = 1;
 
 menuBtn.addEventListener('click',()=>{
         drop.classList.add('show')
@@ -14,3 +18,31 @@ back.addEventListener('click',()=>{
 })
 
 
+function nextslider(){
+  useslider+=1;
+  if(useslider>= slide.length)
+    useslider=0
+initslider()
+}
+
+function prevslider(){
+  useslider-=1;
+  if(useslider < 0)
+  useslider=slide.length-1
+initslider()
+}
+
+
+
+
+function initslider(){
+  slide.forEach(item=>{
+    item.style.opacity= '0';
+})
+
+
+  slide[useslider].style.opacity = '1';
+}
+btnRight.addEventListener('click', nextslider)
+btnleft.addEventListener('click', prevslider)
+initslider()
